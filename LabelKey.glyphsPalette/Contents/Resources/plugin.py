4e5d69602fefb81b801b419dfd52ad17959c01d5
RobertPratley <rob.pratley@gmail.com>
2017-05-15 20:59:13 +0200

    updated LabelKey

--- LabelKey.glyphsPalette/Contents/Resources/plugin.py
+++ LabelKey.glyphsPalette/Contents/Resources/plugin.py
@@ -10,58 +10,29 @@
 #
 #
 ###########################################################################################################
-
-
-from AppKit import NSBezierPath, NSColor, NSFont, NSImage, NSGradient, NSColorSpace, NSMiterLineJoinStyle, NSRoundLineJoinStyle, NSBevelLineJoinStyle, NSFontAttributeName, NSForegroundColorAttributeName, NSGraphicsContext, NSCompositeSourceOver, NSGradientDrawsBeforeStartingLocation, NSGradientDrawsAfterEndingLocation
-from Foundation import NSMakeRect, NSAffineTransform, NSClassFromString, NSMakePoint, NSZeroRect
 from vanilla import *
-from GlyphsApp.plugins import *
-from GlyphsApp.UI import *
 import re
 import os.path
-from os.path import exists
 from os.path import expanduser
+from os.path import exists
+from GlyphsApp.plugins import *
+from GlyphsApp.UI import *
+
+class labelKey(PalettePlugin):
 
 
-class labelKey (PalettePlugin):
-	def assignKeys(self, keyFile):
-		colorLabels = {}
-		global order
-		order = []
-		with open(keyFile) as f:
-			for line in f:
-				color = re.match(r".*?(?=\=)", line).group(0)
-				label = re.search(r"(?<=\=).*", line).group(0)
-				order.append(color)
-				colorLabels[color] = label
-			f.close()
-		# This works	
-		return colorLabels, order
-    
-
-	def settings(self):
-		
+	def settings( self ):
 		self.name = 'Label Key'
-		thisFont = Glyphs.font
-		thisDirPath = os.path.dirname(thisFont.filepath)
-		localKeyFile = thisDirPath + '/labelkey.txt'
-		userKeyFile = '{0}{1}'.format(os.path.abspath(expanduser('~')), '/Library/Application Support/Glyphs/Info/labelkey.txt')
-		if exists(localKeyFile):
-			colorLabels, order = self.assignKeys(localKeyFile)
-		elif exists(userKeyFile):
-			colorLabels, order = self.assignKeys(userKeyFile)
-		height = len(order) * 17
-		self.w = Window((150, height))
-		self.w.frame = Group((0, 0, 150, height))
-		self.w.frame.labels = Group((27, 3, -10, 0))
-		self.w.frame.swatches = CanvasView((10 ,0 ,-10 , 0), self)
-
-
-		for num, i in enumerate(order):
-			setattr(self.w.frame.labels, i, TextBox((0, num * 16, 0, 22), colorLabels[i], sizeStyle="small"))
-
-		self.dialog = self.w.frame.getNSView()
+		width = 160
+		elementHeight = 17
+		self.height = 11 * elementHeight
+		self.paletteView = Window((width, self.height + 10))
+		self.paletteView.frame = Group((0, 0, width, self.height + 10))
 		
+		self.paletteView.frame.swatches = CanvasView((10, 0, -10, 0), self)
+		
+		self.dialog = self.paletteView.frame.getNSView()
+
 	colours = {
 	"red": [0.96, 0.17, 0.17, 1],
 	"orange": [1, 0.6, 0.17, 1],
@@ -76,19 +47,56 @@ class labelKey (PalettePlugin):
 	"lightGray": [0.7, 0.7, 0.7, 1],
 	"charcoal": [0.3, 0.3, 0.3, 1]}
 
-	#def start(self):
-		# Adding a callback for the 'GSUpdateInterface' event
-		
 
-	
 
-	def draw(self, view):
+
+	def update(self, sender):
+		if hasattr(self.paletteView.frame, 'labels'):
+			delattr(self.paletteView.frame, 'labels')
+		colourLabels, order = self.mapKeys(self.setKeyFile())
+		self.paletteView.frame.labels = Group((27, 3, -10, 0))
+		for num, i in enumerate(order):
+			setattr(self.paletteView.frame.labels, i, TextBox((0, num * 16, 0, 22), colourLabels[i], sizeStyle="small"))
+			print i
+
+	def draw( self, view ):
 		keyDiameter = 10
 		height = view.bounds().size.height
+		order = self.mapKeys(self.setKeyFile())[1]
 		for num, i in enumerate(order, 1):
 			r, g, b, a = self.colours[i]
 			NSColor.colorWithCalibratedRed_green_blue_alpha_(r, g, b, a).set()
 			NSBezierPath.bezierPathWithOvalInRect_(((0, height - (num * 16)), (keyDiameter, keyDiameter))).fill()
+
+	def setKeyFile( self ):
+		thisDirPath = os.path.dirname(Glyphs.font.filepath)
+		localKeyFile = thisDirPath + '/labelkey.txt'
+		if exists(localKeyFile):
+			keyFile = localKeyFile
+		else:
+			keyFile = '{0}{1}'.format(os.path.abspath(expanduser('~')), '/Library/Application Support/Glyphs/Info/labelkey.txt')
+		return keyFile
 				
+	def mapKeys( self, keyFile ):
+		order = []
+		colourLabels = {}
+		with open(keyFile) as file:
+			for line in file:
+				colour = re.match(r".*?(?=\=)", line).group(0)
+				label = re.search(r"(?<=\=).*", line).group(0)
+				colourLabels[colour] = label
+				order.append(colour)
+		file.close()
+		print colourLabels, order
+		return colourLabels, order
+	
+
+	def start(self):
+		Glyphs.addCallback(self.update, DOCUMENTACTIVATED)
+
 	def __del__(self):
-		pass
+		Glyphs.removeCallback(self.update)
+
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
